192d359f8dc90aa5e5e2ac7701075dc0f4ed8829
NIYOBYOSE-Isaac-Precieux <isaprecieux112@gmail.com>
2025-12-09 06:06:56 +0200

    Backend setup

---
+++ Backend/brainbridge/brainbridge/src/main/java/com/learn/brainbridge/BrainbridgeApplication.java
@@ -0,0 +1,13 @@
+package com.learn.brainbridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainbridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainbridgeApplication.class, args);
+	}
+
+}
